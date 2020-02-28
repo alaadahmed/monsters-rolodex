b74c890310e5d20ca5d372ffeb198dc57a4988ea
AlaaEldin Ahmed <alaadahmed@gmail.com>
2020-02-29 00:33:34 +0200

    Updates

---
+++ precache-manifest.76200cf35b5d319302c850b0de4eea13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433135c93947bfff0884fba01d2214a5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "29203b3a9f984f5df841",
+    "url": "/monsters-rolodex/static/css/main.a3cfb40f.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29203b3a9f984f5df841",
+    "url": "/monsters-rolodex/static/js/main.5c508a11.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
